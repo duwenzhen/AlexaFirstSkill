0675f2e49e0e4d1e9da3b9681ff6cb0e407703fb
wenzhen <wenzhen.du@gmail.com>
2017-07-15 08:38:52 -0400

    init

---
+++ CrossAssetGame.py
@@ -0,0 +1,54 @@
+import logging
+
+from random import randint
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, statement, question, session
+
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+@ask.launch
+
+def new_game():
+
+    welcome_msg = render_template('welcome')
+
+    return question(welcome_msg)
+
+
+@ask.intent("YesIntent")
+
+def next_round():
+
+    round_msg = render_template('round')
+
+    return question(round_msg)
+
+
+@ask.intent("AnswerIntent", convert={'first': str})
+
+def answer(first):
+
+    winning_answer = "wenzhen"
+
+    if first == winning_answer:
+
+        msg = render_template('win')
+
+    else:
+
+        msg = render_template('lose')
+
+    return statement(msg)
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
